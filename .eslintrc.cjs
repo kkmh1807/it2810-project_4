5bd50fcb47e45888f1a28da65860eea9ffb8f477
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-11-08 09:53:49 +0100

    initial lint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,27 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
+  plugins: ['prettier'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier', 'prettier/prettier'],
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-trailing-spaces': 'error',
+    eqeqeq: ['error', 'smart'],
+    'no-console': 'warn',
+    'no-lonely-if': 'error',
+    'no-nested-ternary': 'error',
+    'no-negated-condition': 'error',
+    'prefer-spread': 'error',
+    'rest-spread-spacing': ['error', 'never']
   }
-}
+};
