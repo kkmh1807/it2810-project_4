153971d7d56fb18a1a294aa1a1577bc8ae3f3528
kkmh1807 <kinekai.holthe@gmail.com>
2022-11-09 18:43:50 +0100

    Add eye component with icons and props

---
+++ src/components/WatchEye.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import EyeUnwatched from './icons/Eye_no_fill.vue';
+import EyeWatched from './icons/Eye_fill.vue';
+
+defineProps<{
+  watched: boolean;
+  movieId?: string;
+}>();
+</script>
+
+<template>
+  <EyeUnwatched class="eye" v-if="!watched" tabIndex="{0}" />
+  <EyeWatched class="eye" v-if="watched" tabIndex="{0}" />
+</template>
+
+<style scoped>
+.eye {
+  cursor: pointer;
+}
+</style>
