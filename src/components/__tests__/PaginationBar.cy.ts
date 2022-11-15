6b2f49cf625343059bb3365b17183cb43494db1e
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-11-15 14:34:21 +0100

    remove pinia testing

--- src/components/__tests__/PaginationBar.cy.ts
+++ src/components/__tests__/PaginationBar.cy.ts
@@ -1,8 +1,6 @@
 import PaginationBar from '../PaginationBar.vue';
 import { createPinia, setActivePinia } from 'pinia';
-// import { usePaginationStore } from '@/stores/pagination';
-// import { createTestingPinia } from '@pinia/testing';
-// import { useMoviesStore } from '@/stores/movies';
+import { useMoviesStore } from '@/stores/movies';
 
 setActivePinia(createPinia());
 
@@ -10,27 +8,22 @@ describe('PaginationBar.cy.ts', () => {
   it('pagination ui works', () => {
     cy.mount(PaginationBar);
 
-    // cy.mount(PaginationBar, { global: { plugins: [createTestingPinia({ createSpy: cy.spy })] } });
-    // const paginationStore = usePaginationStore();
-    // const movieStore = useMoviesStore();
-    // // Set mock-data
-    // movieStore.movieResponse = { data: [], pageInfo: { currentPage: 1, pageSize: 10, totalPages: 10 } };
-    // cy.get('[data-cy="next-page"]').click();
-    // expect(paginationStore.nextPage).to.have.been.called;
+    const movieStore = useMoviesStore();
+    movieStore.movieResponse = { data: [], pageInfo: { currentPage: 1, pageSize: 10, totalPages: 10 } };
 
     cy.get('[data-cy="next-page"]').click();
-    cy.get('[data-cy="current-page"]').contains('2 of 0');
+    cy.get('.pagination-text').contains('2 of 10');
 
     cy.get('[data-cy="last-page"]').click();
-    cy.get('[data-cy="current-page"]').contains('0 of 0');
+    cy.get('.pagination-text').contains('10 of 10');
 
     cy.get('[data-cy="first-page"]').click();
-    cy.get('[data-cy="current-page"]').contains('1 of 0');
+    cy.get('.pagination-text').contains('1 of 10');
 
     cy.get('[data-cy="next-page"]').click();
-    cy.get('[data-cy="current-page"]').contains('2 of 0');
+    cy.get('.pagination-text').contains('2 of 10');
 
     cy.get('[data-cy="prev-page"]').click();
-    cy.get('[data-cy="current-page"]').contains('1 of 0');
+    cy.get('.pagination-text').contains('1 of 10');
   });
 });
